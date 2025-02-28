47137731d415429dea3be20f15afa090f0c260fa
MaramRAlanezi <malanezi24@twq.idserve.net>
2025-02-28 08:35:41 +0300

    Initial Commit

---
+++ Craftify/CraftifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftifyApp.swift
+//  Craftify
+//
+//  Created by Maram Rabeh  on 28/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CraftifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CraftifyTests/CraftifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  CraftifyTests.swift
+//  CraftifyTests
+//
+//  Created by Maram Rabeh  on 28/02/2025.
+//
+
+import Testing
+@testable import Craftify
+
+struct CraftifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
