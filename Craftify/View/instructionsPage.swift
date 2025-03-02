adab5dd3dfa1e0d0cf8269bc4bde4d7de0e8990a
Shaden14 <shadenabdullah14@gmail.com>
2025-03-02 15:27:53 +0300

    instruction page done

---
+++ Craftify/View/instructionsPage.swift
@@ -0,0 +1,18 @@
+//
+//  instructionsPage.swift
+//  Craftify
+//
+//  Created by Shaden Alhumood on 02/09/1446 AH.
+//
+
+import SwiftUI
+
+struct instructionsPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    instructionsPage()
+}
