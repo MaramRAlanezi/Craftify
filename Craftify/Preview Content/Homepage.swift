37e7f6670eb81ceba829bfb91b8a720b08823e8d
Razan66 <raldughaythir24@twq.idserve.net>
2025-03-03 12:41:38 +0300

    Home page complete

---
+++ Craftify/Preview Content/Homepage.swift
@@ -0,0 +1,18 @@
+//
+//  Homepage.swift
+//  Craftify
+//
+//  Created by Razan on 02/09/1446 AH.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homepage()
+}
