f9a7ed2ad69de08d225077e820e2b03c036b341e
Walghamdi97 <wejgh1997@gmail.com>
2025-03-03 23:02:57 +0300

    First Push

---
+++ Craftify/SuppliesView.swift
@@ -0,0 +1,18 @@
+//
+//  SuppliesView.swift
+//  Craftify
+//
+//  Created by Wejdan Alghamdi on 03/09/1446 AH.
+//
+
+import SwiftUI
+
+struct SuppliesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuppliesView()
+}
