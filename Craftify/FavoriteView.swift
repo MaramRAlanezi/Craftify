eb77d0f4fa927183d7c88459c542a04565a52e82
Walghamdi97 <wejgh1997@gmail.com>
2025-03-05 11:27:36 +0300

    Push with Supplies View

---
+++ Craftify/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  Craftify
+//
+//  Created by Wejdan Alghamdi on 04/09/1446 AH.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
